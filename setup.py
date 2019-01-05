5bf7077ca7b04b2fc8519df94c1f04458b855181
mohammad <mohammadsheikhian70@gmail.com>
2019-01-05 16:41:52 +0330

    Adding the requirements

---
+++ setup.py
@@ -0,0 +1,31 @@
+import re
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+
+# reading package version (without loading it)
+with open(join(dirname(__file__), 'auditing', '__init__.py')) as v_file:
+    package_version = re.compile(r".*__version__ = '(.*?)'", re.S)\
+        .match(v_file.read()).group(1)
+
+
+dependencies = [
+    'bddrest',
+]
+
+
+setup(
+    name='audit',
+    version=package_version,
+    description='A ',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # This is important!
+    author='Vahid Mardani',
+    author_email='vahid.mardani@gmail.com',
+    install_requires=dependencies,
+    packages=find_packages(),
+    test_suite='.tests',
+    license='MIT',
+
+)
+
